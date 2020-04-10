5985a7cb7e7e1e7054df9fed81e1f2cc19457d2b
Iuliia Plekhanova <julec2006@yandex.ru>
2020-04-10 23:10:01 +0400

    feat: add webpack config, burger menu

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import '../css/style.css';
+import '../css/style.scss';
+import {
+  cards
+} from './cards';
+
+import {
+  menuBtn
+} from './burger-menu';
+
+
+
+console.log(cards);
+console.log(menuBtn);
